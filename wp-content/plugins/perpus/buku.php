540d8f120b8b902a44b8b802bd5146eebe584e22
Allan <allan.080904@gmail.com>
2025-06-19 15:46:16 +0700

    Update buku

--- wp-content/plugins/perpus/buku.php
+++ wp-content/plugins/perpus/buku.php
@@ -52,7 +52,7 @@ $result = $conn->query($sql);
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <div class="container mt-4">
     <h3 class="mb-3">Daftar Buku</h3>
-    <a href="admin.php?page=utama&panggil=tambah-buku.php" class="btn btn-primary mb-3">Tambah Buku</a>
+    <a href="admin.php?page=perpus_utama&panggil=tambah_buku.php" class="btn btn-primary mb-3">Tambah Buku</a>
 
     <!-- Formulir Edit -->
     <?php if ($editData): ?>
