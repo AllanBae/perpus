d8a57b39eeae47cbf5e3000e636799b2944d27ae
Allan <allan.080904@gmail.com>
2025-06-25 18:04:17 +0700

    up

--- wp-content/plugins/perpus/utama.php
+++ wp-content/plugins/perpus/utama.php
@@ -8,34 +8,64 @@
 */
 
 function perpus_modulku() {
-    global $conn;
+    $plugin_url = plugin_dir_url(__FILE__);
+
     // Buat koneksi database sekali, pakai konstanta WP untuk host dan user database
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
     if ($conn->connect_error) {
         die("Koneksi gagal: " . $conn->connect_error);
     }
 
-    ?>
+?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <style>
+        #wpcontent {
+            padding: 0 !important;
+        }
+
+        .form-select {
+            width: 100% !important;
+        }
+    </style>
 
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img width="20%" src="<?= plugin_dir_url(__FILE__) ?>/pp1.png"></a>
+            <a class="navbar-brand" href="#">
+                <img src="<?= $plugin_url; ?>/pp1.png" alt="" style="width: 65px; height: 65px; border-radius: 50%;">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" href="admin.php?page=perpus_utama&panggil=kategori.php">Entry Data Kategori</a>
+                        <a class="nav-link" href="admin.php?page=perpus_utama&panggil=kategori.php">Entry Data Kategori</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="admin.php?page=perpus_utama&panggil=buku.php">Entry Data Buku</a>
+                        <a class="nav-link" href="admin.php?page=perpus_utama&panggil=buku.php">Entry Data Buku</a>
                     </li>
-                    <!-- Tambah menu lain di sini jika perlu -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php?page=perpus_utama&panggil=pengunjung.php">Entry Data Pengunjung</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Master</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="admin.php?page=utama&panggil=jenis.php">Manajemen Jenis Keluhan</a></li>
+                            <li><a class="dropdown-item" href="admin.php?page=utama&panggil=bagian.php">Bagian</a></li>
+                            <li><a class="dropdown-item" href="admin.php?page=utama&panggil=mahasiswa.php">Mahasiswa</a></li>
+                            <li><a class="dropdown-item" href="#">A third link</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Dropdown</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Link</a></li>
+                            <li><a class="dropdown-item" href="#">Another link</a></li>
+                            <li><a class="dropdown-item" href="#">A third link</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -56,7 +86,7 @@ function perpus_modulku() {
         }
         ?>
     </div>
-    <?php
+<?php
 }
 
 function perpus_tambah_menu() {
@@ -67,9 +97,8 @@ function perpus_tambah_menu() {
         'perpus_utama',                    // Menu slug
         'perpus_modulku',                  // Callback function
         'dashicons-book-alt',      // Icon
-        25,                         // Position
+        81,                         // Position
     );
-
 }
 add_action('admin_menu', 'perpus_tambah_menu');
 
