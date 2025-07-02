8b6fcdc899d523c5778770e9f582636df50df06a
Allan <allan.080904@gmail.com>
2025-07-02 23:51:30 +0700

    edit

--- wp-content/plugins/perpus/tambah_denda.php
+++ wp-content/plugins/perpus/tambah_denda.php
@@ -2,25 +2,37 @@
 $conn = new mysqli("localhost", "root", "", "db_ti6b_uas");
 if ($conn->connect_error) die("Koneksi gagal: " . $conn->connect_error);
 
-// Ambil pengembalian yang status_denda = 'terdenda' dan belum masuk denda
+// Ambil pengembalian yang status_denda = 'terdenda' dan belum masuk tabel denda
 $pengembalian = $conn->query("SELECT no_pengembalian FROM pengembalian 
                               WHERE status_denda = 'terdenda' 
                               AND no_pengembalian NOT IN (SELECT no_pengembalian FROM denda)");
+
+// Fungsi generate kode denda otomatis
+function generateNoDenda($conn) {
+    $result = $conn->query("SELECT no_denda FROM denda ORDER BY no_denda DESC LIMIT 1");
+    if ($result->num_rows > 0) {
+        $last = $result->fetch_assoc()['no_denda'];
+        $num = (int)substr($last, 2); // Ambil angka setelah 'DN'
+        $num++;
+        return 'DN' . str_pad($num, 2, '0', STR_PAD_LEFT);
+    } else {
+        return 'DN01';
+    }
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $no_pengembalian = $_POST['no_pengembalian'];
     $tarif = $_POST['tarif_denda'];
     $alasan = $_POST['alasan_denda'];
     $tgl = date('Y-m-d');
-    $no_denda = "DN" . time();
+    $no_denda = generateNoDenda($conn);
 
     $conn->query("INSERT INTO denda (no_denda, tgl_denda, tarif_denda, alasan_denda, no_pengembalian) 
                   VALUES ('$no_denda', '$tgl', '$tarif', '$alasan', '$no_pengembalian')");
 
-    // Update status denda
     $conn->query("UPDATE pengembalian SET status_denda='terdenda' WHERE no_pengembalian='$no_pengembalian'");
 
-    echo "<script>alert('Denda ditambahkan');location.href='admin.php?page=perpus_utama&panggil=denda.php';</script>";
+    echo "<script>alert('Denda berhasil ditambahkan');location.href='admin.php?page=perpus_utama&panggil=denda.php';</script>";
 }
 ?>
 
@@ -33,10 +45,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <option value="<?= $r['no_pengembalian'] ?>"><?= $r['no_pengembalian'] ?></option>
         <?php endwhile; ?>
     </select>
-    <label>Tarif Denda</label>
+    <label class="mt-2">Tarif Denda</label>
     <input type="number" name="tarif_denda" required class="form-control">
-    <label>Alasan Denda</label>
-    <input type="text" name="alasan_denda" required class="form-control mt-1">
+    <label class="mt-2">Alasan Denda</label>
+    <input type="text" name="alasan_denda" required class="form-control">
     <br>
     <button type="submit" class="btn btn-success">Simpan</button>
 </form>
