fe7febcfe7eec02350363fe5ef855a9bdbc2b15a
Allan <allan.080904@gmail.com>
2025-07-02 23:13:41 +0700

    edit

--- wp-content/plugins/perpus/pengembalian.php
+++ wp-content/plugins/perpus/pengembalian.php
@@ -4,7 +4,7 @@ $conn = new mysqli("localhost", "root", "", "db_ti6b_uas");
 if ($conn->connect_error) die("Koneksi gagal: " . $conn->connect_error);
 
 // Query pengembalian + anggota + status
-$sql = "SELECT p.*, a.nm_anggota FROM pengembalian p
+$sql = "SELECT p.*, a.nm_anggota, a.id_anggota FROM pengembalian p
         LEFT JOIN peminjaman pm ON p.no_peminjaman = pm.no_peminjaman
         LEFT JOIN anggota a ON pm.id_anggota = a.id_anggota
         ORDER BY p.no_pengembalian DESC";
@@ -19,20 +19,20 @@ $result = $conn->query($sql);
     <tr>
       <th>No</th>
       <th>No Pengembalian</th>
+      <th>ID Anggota</th>
       <th>Nama Anggota</th>
       <th>Tanggal Pengembalian</th>
-      <th>Status Pengembalian</th>
       <th>Status Denda</th>
     </tr>
   </thead>
   <tbody>
-    <?php $no=1; while($row = $result->fetch_assoc()): ?>
+    <?php $no = 1; while($row = $result->fetch_assoc()): ?>
     <tr>
       <td><?= $no++ ?></td>
       <td><?= $row['no_pengembalian'] ?></td>
+      <td><?= $row['id_anggota'] ?></td>
       <td><?= $row['nm_anggota'] ?></td>
       <td><?= $row['tgl_pengembalian'] ?></td>
-      <td><?= ucfirst($row['status_pengembalian']) ?></td>
       <td><?= ucfirst($row['status_denda']) ?></td>
     </tr>
     <?php endwhile; ?>
